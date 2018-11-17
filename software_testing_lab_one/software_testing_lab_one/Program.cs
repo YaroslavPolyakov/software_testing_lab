e1628b76846a17b66ce55919dd4a4113284a823b
Yarolav Polyakov <sensored79@gmail.com>
2018-11-17 22:20:17 +0300

    change generic file

--- software_testing_lab_one/software_testing_lab_one/Program.cs
+++ software_testing_lab_one/software_testing_lab_one/Program.cs
@@ -17,7 +17,7 @@ namespace Laba_1
             //commit number one
             //commit number one in develop branch
             //commit number two
-            //commit number two in develop branch
+            //change generic file
             //commit number three
 
             foreach (var item in students)
